9e59ed3b901cacbf0d3b4ae625395f1b64426d7f
dell <dell@DESKTOP-IN0A9H5>
2021-10-19 13:15:22 +0530

    Six commit

---
+++ GoLeor_Framework/src/test/java/com/crm/ContactTest/ContactTest.java
@@ -0,0 +1,16 @@
+package com.crm.ContactTest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void createContactTest()
+	{
+		System.out.println("execute createcontacttest");
+	}
+	@Test
+	public void editContactTest()
+	{
+		System.out.println("execute editcontacttest");
+	}
+}
---
+++ GoLeor_Framework/src/test/java/com/crm/orgTest/orgTest.java
@@ -0,0 +1,13 @@
+package com.crm.orgTest;
+
+import org.testng.annotations.Test;
+@Test
+public class orgTest 
+{
+
+	public void createorgTest()
+	{
+		System.out.println();
+	}
+}
+
